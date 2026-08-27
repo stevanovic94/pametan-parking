51590b7a63f4dc11e72b5b132379775abf7dc749
stevanovic94 <djolestevanovic94@gmail.com>
2026-08-27 15:13:56 +0200

    Dodate Reactive Forms i validacija prijave

--- mobile/src/app/features/authentication/login/login.page.ts
+++ mobile/src/app/features/authentication/login/login.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { 
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 // uvoz komponenti iz Ionic biblioteke
 import { IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from '@ionic/angular';
@@ -10,13 +14,35 @@ import { IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   //dozvole komponentama da koriste se koriste u HTML-u stranici 
-  imports: [IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]                                  
+  imports: [IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule, RouterLink]                                  
 })
-export class LoginPage implements OnInit {
+export class LoginPage{
 
-  constructor() { }
+  private readonly formBuilder = inject(FormBuilder);
 
-  ngOnInit() {
+  readonly loginForm = this.formBuilder.nonNullable.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+      ]
+    ]
+  });
+
+  onSubmit(): void {
+    if(this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Forma za prijavu je validna.');
   }
 
 }
