925991985ebcfc4cfdefad1e3da9387bb6fd910f
stevanovic94 <djolestevanovic94@gmail.com>
2026-08-27 12:59:33 +0200

    Dodat osnovni interfejs stranice za prijavu

--- mobile/src/app/features/authentication/login/login.page.ts
+++ mobile/src/app/features/authentication/login/login.page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import { IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from '@ionic/angular';
+                                //uvoz IonInput iz Ionic biblioteke
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar, CommonModule, FormsModule]
+                                  //dozvoli LoginPage komponenti da koristi IonInput u svom HTML-u
 })
 export class LoginPage implements OnInit {
 
