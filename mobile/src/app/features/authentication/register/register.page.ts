47fa7525acc4c91b576c7ce1d64775e603d3c5a3
stevanovic94 <djolestevanovic94@gmail.com>
2026-08-27 15:00:07 +0200

    Dodate Reactive Forms i validacija registracije

--- mobile/src/app/features/authentication/register/register.page.ts
+++ mobile/src/app/features/authentication/register/register.page.ts
@@ -1,20 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { 
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,  
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+ } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from '@ionic/angular';
+
+const passwordsMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+
+  return password === confirmPassword ? null : { passwordsMismatch: true };
+};
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  imports: [IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar, ReactiveFormsModule, RouterLink, CommonModule]
 })
-export class RegisterPage implements OnInit {
 
-  constructor() { }
+export class RegisterPage {
 
-  ngOnInit() {
+  private readonly formBuilder = inject(FormBuilder);
+
+  readonly registerForm = this.formBuilder.nonNullable.group(
+    {
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50)
+        ]
+      ],
+
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50)
+        ]
+      ],
+
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8)
+        ]
+      ],
+
+      confirmPassword: [
+        '',
+        [
+          Validators.required
+        ]
+      ]
+    },
+    {
+      validators: [passwordsMatchValidator]
+    }
+  );
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Forma za registraciju je validna.');
   }
-
 }
